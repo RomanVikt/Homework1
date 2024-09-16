f806b8fed0a4b3ff1bb27a5fab6f86392fe71c52
RomanVikt <romkam@mail.ru>
2024-09-16 20:30:39 +1200

    revision 4

---
+++ hw-1.js
@@ -0,0 +1,66 @@
+/* Первое задание */
+
+let h = 10;
+alert(h);
+h = 20;
+alert(h);
+
+/* Второе задание */
+
+const firstYear = 2007;
+alert(firstYear);
+
+/* Третье задание */
+
+const creatorName = 'Brendan Eich';
+alert(creatorName);
+
+/* Четвертое задание */
+
+let ten = 10;
+let two = 2;
+alert(ten + two);
+alert(ten - two);
+alert(ten * two);
+alert(ten / two);
+
+/* Пятое задание */
+
+let result = 2 ** 5;
+alert(result);
+
+/* Шестое задание */
+
+let a = 9;
+let b = 2;
+let ostatok = 9 % 2;
+alert(ostatok);
+
+/* Седьмое задание */
+
+let num = 1;
+num += 5;
+num -= 3;
+num *= 7;
+num /= 3;
+num = num++;
+num = num--;
+alert(num);
+
+/* Восьмое задание */
+
+let age = prompt("Сколько вам лет?");
+alert(age);
+
+/* Девятое задание */
+
+const user = {
+    name: "Jonny",
+    age: 34,
+    isAdmin: true
+}
+
+/* Десятое задание */
+
+const name = prompt("Как тебя зовут?");
+alert("Привет , " + name + " !");  // Не пойму почему перечеркивает name, в codepen все работает!
