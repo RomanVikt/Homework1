50c283f28d432e314afa1b5e5c15c32fafe4daed
RomanVikt <romkam@mail.ru>
2024-09-29 08:38:34 +1200

    revision 8

--- index.js
+++ index.js
@@ -1,25 +1,22 @@
-
-    const min = 1;
-    const max = 100;
-    const randomNum = Math.floor(Math.random() * (max - min + 1)) + min;
+function game1() {
+  const min = 1;
+  const max = 100;
+  const randomNum = Math.floor(Math.random() * (max - min + 1)) + min;
     
-    alert (randomNum); //Загаданное число
+  alert (randomNum); //Загаданное число
     
-    let userNum = +prompt("Угадай число от 1 до 100! Укажи свой вариант:");
+  let userNum = +prompt("Угадай число от 1 до 100! Укажи свой вариант:");
     
-    if (userNum > 100 || userNum < 1)
-          {
-            alert("Введите коректное число");
-          }
-     else if (userNum > randomNum)
-          {
-            alert("Загаданное число меньше"); 
-          }
-     else if (userNum < randomNum)
-          {
-            alert("Загаданное число больше");
-          }
-     else if (userNum === randomNum)
-          {
-            alert("Угадал.Молодец!");
-          }
+  if (userNum > 100 || userNum < 1) {
+    alert("Введите коректное число");
+    }
+  else if (userNum > randomNum) {
+    alert("Загаданное число меньше"); 
+    }
+  else if (userNum < randomNum) {
+    alert("Загаданное число больше");
+    }
+  else (userNum === randomNum) {
+    alert("Угадал.Молодец!");
+    }
+}
